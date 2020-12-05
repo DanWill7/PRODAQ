72a8bc67cd0f37980f0f11382aefde9cc8b58bcc
dmw7 <dmw7@iastate.edu>
2020-12-05 17:44:42 -0600

    Made functioning main and addded requirements.txt

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-# PRODAQ Main File
+# Main file to run UDFs for PRODAQ the application
+# Author: Daniel Williams
+# Date Created: 12/5/2020 5:42PM
+
+def main():
+    print("Finished")
+
+
+if __name__ == "__main__":
+    main()
